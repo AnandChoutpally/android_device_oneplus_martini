2f3229a9d899fc090cad0b30d273a6f19d97e295
Anand Choutpally <91369446+AnandChoutpally@users.noreply.github.com>
2026-02-04 00:23:22 +0530

    Update device inheritance and product details for martini

--- twrp_martini.mk
+++ twrp_martini.mk
@@ -8,25 +8,25 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
-# Inherit from lemonadep device
-$(call inherit-product, device/oneplus/lemonadep/device.mk)
+# Inherit from martini device
+$(call inherit-product, device/oneplus/martini/device.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-PRODUCT_NAME := twrp_lemonadep
-PRODUCT_DEVICE := lemonadep
+PRODUCT_NAME := twrp_martini
+PRODUCT_DEVICE := martini
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
-PRODUCT_MODEL := LE2125
+PRODUCT_MODEL := MT2111
 
-PRODUCT_SYSTEM_NAME := OnePlus9Pro
-PRODUCT_SYSTEM_DEVICE := OnePlus9Pro
+PRODUCT_SYSTEM_NAME := OnePlus9RT
+PRODUCT_SYSTEM_DEVICE := OnePlus9RT
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    TARGET_DEVICE=OnePlus9Pro \
-    TARGET_PRODUCT=OnePlus9Pro
+    TARGET_DEVICE=OnePlus9RT \
+    TARGET_PRODUCT=OnePlus9RT
 
-BUILD_FINGERPRINT := OnePlus/OnePlus9Pro/OnePlus9Pro:12/RKQ1.211119.001/R.202210251512:user/release-keys
+BUILD_FINGERPRINT := OnePlus/OnePlus9RT/OnePlus9RT:12/RKQ1.211119.001/R.202210251512:user/release-keys
